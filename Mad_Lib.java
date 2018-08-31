d87bb79e2b261981d2068a227cc0b35347291b28
Schogan <navy15@gmail.com>
2018-08-30 21:18:40 -0500

    Initial commit

---
+++ Mad_Lib.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class HelloWorld {
+	public static void main(String[] args){
+		/*int myAge =31;
+		char mySex ='M';
+		double myHeight = 6;
+		String name;
+		Scanner inputDevice = new Scanner(System.in);
+		
+		System.out.println("Hello World");
+		System.out.println("I am: " + myAge + " years old");
+		System.out.println("I am: " + mySex);
+		System.out.print("My height is: " + myHeight + "ft\n");
+		System.out.println("What is your name?");
+		name = inputDevice.nextLine();
+		System.out.println("Hello " + name + " welcome to class.");*/
+	}
+}
