e58039937e9a792cc4b6801bfc161c2110b815c6
Schogan <navy15@gmail.com>
2018-09-01 12:32:44 -0500

    minor corrections

--- Mad_Lib.java
+++ Mad_Lib.java
@@ -64,14 +64,14 @@ public class Mad_Lib {
 		System.out.println(" around for everyone to see. It must have been " + fltA);
 		System.out.println(" feet long and " + fltB + " feet wide! ");
 		System.out.println("Everywhere that they took it people would say it made ");
-		System.out.println("them think of a " + nounC + "from another world. ");
+		System.out.println("them think of a " + nounC + " from another world. ");
 		System.out.println("Astounded, people would " + verbB + " and " + verbC);
 		System.out.println(" as the " + adjA + " " + nounA + " passed. ");
 		System.out.println("One day a " + adjB + " " + nounD + " descended on the country and the ");
 		System.out.println(adjA + " " + nounA + " that was loved by so many was " + verbD);
 		System.out.println(" and would never be seen again. Until a " + adjC + " hero named ");
 		System.out.println(nameA + " came to the rescue and " + verbE + " the ");
-		System.out.println(adjA + " " + nounA + " restoring it to its’ former glory. ");
+		System.out.println(adjA + " " + nounA + " restoring it to its former glory. ");
 		System.out.println(nameA + " was then taken to " + placeA);
 		System.out.println(" where they would be " + verbF);
 		System.out.println(" forever and the country can rejoice again.");
